bc1c404fdd15b01f16383895d92dfe9e4f8a3140
Christopher <hello@blick-labs.com>
2015-09-15 12:50:18 -0700

    Initial Commit

---
+++ RNBeacons/RNBeacons.h
@@ -0,0 +1,13 @@
+//
+//  RNBeacons.h
+//  RNBeacons
+//
+//  Created by Christopher on 9/15/15.
+//  Copyright (c) 2015 Blick Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNBeacons : NSObject
+
+@end
---
+++ RNBeacons/RNBeacons.m
@@ -0,0 +1,13 @@
+//
+//  RNBeacons.m
+//  RNBeacons
+//
+//  Created by Christopher on 9/15/15.
+//  Copyright (c) 2015 Blick Labs. All rights reserved.
+//
+
+#import "RNBeacons.h"
+
+@implementation RNBeacons
+
+@end
